8eb5242975ab3243d608c2f9a4b74c4d187e70c6
Mangesh Lolage <lolagemangesh2003@gmail.com>
2026-05-13 10:00:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const student = {
+  name: "John",
+};
+
+function getKeys(obj) {
+  let arr = [];
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      arr.push(key);
+    }
+  }
+  return arr;
+}
